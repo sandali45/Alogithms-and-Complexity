d681ea01569838bba10dbe90e1fadba72f499a51
sandali45 <139307298+sandali45@users.noreply.github.com>
2024-09-29 22:24:47 +0530

    Create DFS

---
+++ BruteForce/DFS.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BruteForce
+{
+    public class DFS
+    {
+        public static void AddEdge(List<List<int>> adj, int s, int t)
+        {
+            adj[s].Add(t);
+            adj[t].Add(s);
+        }
+
+        // Recursive function for DFS traversal
+        public static void DFSRec(List<List<int>> adj, bool[] visited, int s)
+        {
+            // Mark the current vertex as visited
+            visited[s] = true;
+
+            // Print the current vertex
+            Console.Write(s + " ");
+
+            // Recursively visit all adjacent vertices
+            // that are not visited yet
+            foreach (int i in adj[s])
+            {
+                if (!visited[i])
+                {
+                    DFSRec(adj, visited, i);
+                }
+            }
+        }
+
+        // Main DFS function that initializes the visited array
+        public static void PerformDFS(List<List<int>> adj, int s)
+        {
+            bool[] visited = new bool[adj.Count];
+            // Call the recursive DFS function
+            DFSRec(adj, visited, s);
+        }
+        }
+}
--- BruteForce/Program.cs
+++ BruteForce/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace BruteForce
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            dfs();
+        }
+
+        public  static void dfs()
+        {
+            int V = 5;
+
+            // Create an adjacency list for the graph
+            List<List<int>> adj = new List<List<int>>(V);
+            for (int i = 0; i < V; i++)
+            {
+                adj.Add(new List<int>());
+            }
+
+            // Define the edges of the graph
+            int[,] edges = {
+            { 1, 2 }, { 1, 0 }, { 2, 0 }, { 2, 3 }, { 2, 4 }
+        };
+
+            // Populate the adjacency list with edges
+            for (int i = 0; i < edges.GetLength(0); i++)
+            {
+                DFS.AddEdge(adj, edges[i, 0], edges[i, 1]);
+            }
+
+            int source = 1; // Starting vertex for DFS
+            Console.WriteLine("DFS from source: " + source);
+            DFS.PerformDFS(adj, source);
+        }
+    }
+}
