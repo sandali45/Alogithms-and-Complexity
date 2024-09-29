94be00a042af7cb444bb71c6d95bec081b1cb8c8
sandali45 <139307298+sandali45@users.noreply.github.com>
2024-09-29 12:18:37 +0530

    Add Bubble sort

---
+++ Sorting Algorithms/BubbleSort.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_Algorithms
+{
+    public class BubbleSort
+    {
+        // An optimized version of Bubble Sort
+        public static void bubbleSort(int[] arr, int n)
+        {
+            int i, j, temp;
+            bool swapped;
+            for (i = 0; i < n - 1; i++)
+            {
+                swapped = false;
+                for (j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+
+                        // Swap arr[j] and arr[j+1]
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                // If no two elements were
+                // swapped by inner loop, then break
+                if (swapped == false)
+                    break;
+            }
+        }
+
+        // Function to print an array
+       public static void printArray(int[] arr, int size)
+        {
+            int i;
+            for (i = 0; i < size; i++)
+                Console.Write(arr[i] + " ");
+            Console.WriteLine();
+        }
+
+    }
+}
--- Sorting Algorithms/Program.cs
+++ Sorting Algorithms/Program.cs
@@ -4,13 +4,33 @@
     {
        public  static void Main(string[] args)
         {
+            selectionsort();
+            bubblesort();
+        }
+
+        public static void selectionsort()
+        {
             int[] arr = { 64, 25, 12, 22, 11 };
             Console.WriteLine("Original Array:");
             SelectionSort.printArray(arr);
 
             SelectionSort.selectionSort(arr);
-            Console.WriteLine("Sorted Array:");
+            Console.WriteLine("Selection sorted - Sorted Array:");
             SelectionSort.printArray(arr);
+            Console.WriteLine("...................................................................");
+        }
+
+        public static void bubblesort()
+        {
+            int[] arr = { 5,1,3,6 };
+            int n = arr.Length;
+
+            Console.WriteLine("Original Array:");
+            BubbleSort.printArray(arr, n);
+
+            BubbleSort.bubbleSort(arr, n);
+            Console.WriteLine("Bubble sorted - Sorted Array:");
+            BubbleSort.printArray(arr, n);
         }
     }
 }
