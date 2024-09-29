60b30dfe6b8e199bb332645bc9a8dfc9918607b5
sandali45 <139307298+sandali45@users.noreply.github.com>
2024-09-29 22:33:51 +0530

    Add BFS

---
+++ BruteForce/BFS.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BruteForce
+{
+    public class BFS
+    {
+        public static void Bfs(List<List<int>> adj, int s)
+        {
+
+            // Create a queue for BFS
+            Queue<int> q = new Queue<int>();
+
+            // Mark all the vertices as not visited
+            bool[] visited = new bool[adj.Count];
+
+            // Mark the source node as visited and enqueue it
+            visited[s] = true;
+            q.Enqueue(s);
+
+            // Iterate over the queue
+            while (q.Count > 0)
+            {
+
+                // Dequeue a vertex from queue and print it
+                int curr = q.Dequeue();
+                Console.Write(curr + " ");
+
+                // Get all adjacent vertices of the dequeued vertex
+                // If an adjacent has not been visited, 
+                // mark it visited and enqueue it
+                foreach (int x in adj[curr])
+                {
+                    if (!visited[x])
+                    {
+                        visited[x] = true;
+                        q.Enqueue(x);
+                    }
+                }
+            }
+        }
+
+        // Function to add an edge to the graph
+        public static void AddEdge(List<List<int>> adj,
+                            int u, int v)
+        {
+            adj[u].Add(v);
+            adj[v].Add(u);
+        }
+
+    }
+}
--- BruteForce/Program.cs
+++ BruteForce/Program.cs
@@ -5,6 +5,8 @@
         public static void Main(string[] args)
         {
             dfs();
+            Console.WriteLine("........................................................");
+            bfs();
         }
 
         public  static void dfs()
@@ -32,6 +34,32 @@
             int source = 1; // Starting vertex for DFS
             Console.WriteLine("DFS from source: " + source);
             DFS.PerformDFS(adj, source);
+          
+        }
+
+        public static void bfs()
+        {
+           
+            // Number of vertices in the graph
+            int V = 5;
+
+            // Adjacency list representation of the graph
+            List<List<int>> adj = new List<List<int>>(V);
+            for (int i = 0; i < V; i++)
+            {
+                adj.Add(new List<int>());
+            }
+
+            // Add edges to the graph
+            BFS.AddEdge(adj, 0, 1);
+            BFS.AddEdge(adj, 0, 2);
+            BFS.AddEdge(adj, 1, 3);
+            BFS.AddEdge(adj, 1, 4);
+            BFS.AddEdge(adj, 2, 4);
+
+            // Perform BFS traversal starting from vertex 0
+            Console.WriteLine("BFS starting from 0 : ");
+            BFS.Bfs(adj, 0);
         }
     }
 }
